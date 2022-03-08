e2227cd11bf7be465f1973ba63109eabf8bec4ac
navyatheinquisitive <101185170+navyatheinquisitive@users.noreply.github.com>
2022-03-08 22:48:44 +0530

    Update Q13.cpp

--- Q13.cpp
+++ Q13.cpp
@@ -16,20 +16,29 @@ int main()
     ofstream f2;
     f2.open("Question_13_output.txt");
 
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 5; i++)
     {
         Student obj;
-
+        cout << "Roll No     : ";
         cin >> obj.RollNo;
         f2 << obj.RollNo << "  ";
 
+        cout << "Name        : ";
         cin.ignore();
         getline(cin, obj.Name);
         f2 << obj.Name << "    ";
+        
+        cout << "Class       : ";
+        cin >> obj.Class;
 
-        cin >> obj.Class >> obj.Year >> obj.TotalMarks;
+        cout << "Year        : ";
+        cin >> obj.Year;
+
+        cout << "Total Marks : ";
+        cin >> obj.TotalMarks;
+
         f2 << obj.Class << "  " << obj.Year << "  " << obj.TotalMarks << endl;
-    }
+        }
 
     return 0;
 }
